de86b319e8e763ddc2617387938ed6ad0c3a82d0
Atchaya1998 <109141836+Atchaya1998@users.noreply.github.com>
2022-11-08 17:53:50 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-ldpa2a
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-ldpa2a)
---
+++ src/app/ticketbooking/ticketbooking.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit } from '@angular/core';
+import { Ticketbooking } from './ticketbooking';
+
+import { TicketbookingService } from './ticketbooking.service';
+
+@Component({
+  selector: 'app-ticketbooking',
+  templateUrl: './ticketbooking.component.html',
+  styleUrls: ['./ticketbooking.component.css'],
+})
+export class TicketbookingComponent implements OnInit {
+  ticketbooking = [];
+  model = new Ticketbooking();
+
+  constructor(private ticketbookingService: TicketbookingService) {}
+
+  ngOnInit() {
+    this.getAllTicketbooking();
+  }
+
+  public getAllTicketbooking() {
+    this.ticketbookingService
+      .getAllTicketbookingService()
+      .subscribe((x: any[]) => {
+        this.ticketbooking = x;
+      });
+  }
+
+  editTicketbooking(id: string) {
+    alert(id);
+    this.ticketbookingService
+      .getTicketbookingService(id)
+      .subscribe((data: any) => {
+        this.model = data;
+      });
+    // alert(JSON.stringify(this.model));
+  }
+
+  // deleteTicketbooking(id: any) {
+  //   alert(JSON.stringify(this.model));
+  //   this.ticketbookingService
+  //     .deleteTicketbookingService(id)
+  //     .subscribe((data) => {
+  //       this.getAllTicketbooking();
+  //     });
+  //   alert(JSON.stringify(this.model));
+  // }
+  deleteTicketbooking(id: any) {
+    alert(id);
+    this.ticketbookingService
+      .deleteTicketbookingService(id)
+      .subscribe((data) => {
+        this.getAllTicketbooking();
+      });
+  }
+
+  addTicketbooking() {
+    alert(JSON.stringify(this.model));
+    if (!this.model.id) {
+      // alert(JSON.stringify(this.model));
+      this.ticketbookingService
+        .createTicketbookingService(this.model)
+        .subscribe((data) => {
+          this.getAllTicketbooking();
+          this.model = new Ticketbooking();
+        });
+    } else {
+      // alert(JSON.stringify(this.model));
+      this.ticketbookingService
+        .updateTicketbookingService(this.model.id, this.model)
+        .subscribe((data) => {
+          this.getAllTicketbooking();
+          this.model = new Ticketbooking();
+        });
+    }
+  }
+}
